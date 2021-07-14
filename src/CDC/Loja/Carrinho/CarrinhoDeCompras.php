1fab9d4a94611624b3fcceca0e55b584f93afa07
João Marcus <joaomarcus12769@gmail.com>
2021-07-14 19:44:34 -0300

    base: Produto e CarrinhoDeCompras

---
+++ src/CDC/Loja/Carrinho/CarrinhoDeCompras.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace CDC\Loja\Carrinho;
+
+
+use ArrayObject;
+use CDC\Loja\Produto\Produto;
+
+class CarrinhoDeCompras
+{
+    private ArrayObject $produtos;
+
+    public function __construct()
+    {
+        $this->produtos = new ArrayObject();
+    }
+
+    public function adiciona(Produto $produto)
+    {
+        $this->produtos->append($produto);
+        return $this;
+    }
+
+    /**
+     * @return \ArrayObject
+     */
+    public function getProdutos(): \ArrayObject
+    {
+        return $this->produtos;
+    }
+
+    public function maiorValor()
+    {
+        if (count($this->getProdutos() === 0)) {
+            return 0;
+        }
+
+        $maiorValor = $this->getProdutos()[0]->getValor();
+        foreach ($this->getProdutos() as $produto) {
+            if($maiorValor < $produto->getValor()){
+                $maiorValor = $produto->getValor();
+            }
+
+            return $maiorValor;
+        }
+    }
+}
